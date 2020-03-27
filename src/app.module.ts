599266d1cbcd4590dcb493014002645b117b1164
luisdev97 <amadormontorodavid@gmail.com>
2020-03-27 19:12:32 +0100

    GraphQLModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      playground: true
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
